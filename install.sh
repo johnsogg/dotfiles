a08336c51dfa157c7fd16fb508433b1a645f4932
Gabe Johnson <gabe@blankslatesystems.com>
2013-11-27 18:08:20 -0700

    added an install script.

---
+++ install.sh
@@ -0,0 +1,41 @@
+# 
+# install.sh
+#
+# This 'installs' files by first removing existing versions and then
+# linking them to the versions provided in this little repository.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+remove_files ()
+{
+    echo "Removing existing files..."
+    rm -fv ~/.bash_profile
+    rm -fv ~/.bashrc
+}
+
+link_files ()
+{
+    echo "Linking files..."
+    ln -sv $DIR/bash_profile ~/.bash_profile
+    ln -sv $DIR/bashrc ~/.bashrc
+}
+
+
+echo "This will erase your ~/.bash_profile and ~/.bashrc files"
+echo "and create symlinks to the versions in this directory:"
+echo $DIR
+echo ""
+echo "Do you want to continue?"
+
+select yn in "Yes" "No"; do
+case $yn in
+    Yes )
+	remove_files;
+	link_files;
+	echo "Done.";
+	exit;;
+    No )
+	echo "Not installing. Have a nice day.";
+	exit;;
+esac
+done
